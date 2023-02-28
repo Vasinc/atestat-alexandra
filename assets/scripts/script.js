d43fb3c2c2be8ae9631f57bd5db6a3a1592abb89
Vasinc <lein0.jade@gmail.com>
2023-02-28 22:59:33 +0200

    2 ( header added )

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,23 @@
-console.log('succes')
+const burgerMenu = document.querySelector('.burger-menu');
+const headerUl = document.querySelector('ul');
+const backdrop = document.getElementById('backdrop');
+
+
+function handleHeaderUl () {
+    burgerMenu.classList.toggle('active-burger')
+    backdrop.classList.toggle('display-block')
+    headerUl.classList.toggle('display-flex')
+    setTimeout(() => {
+        headerUl.classList.toggle('width-70')
+    }, 1);
+}
+
+burgerMenu.addEventListener('click', handleHeaderUl)
+
+backdrop.addEventListener('click', handleHeaderUl)
+
+addEventListener('resize', () => {
+    if (innerWidth >= 800 && backdrop.classList.contains('display-block')) {
+        handleHeaderUl()
+    }
+})
